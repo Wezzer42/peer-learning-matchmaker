1bba6be34f563aa5e4127d101d5d8442c5cab186
Wezzer42 <alibaev.am5@gmail.com>
2025-09-12 01:04:11 +0900

    Fixed build

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  "include": [
-    "next-env.d.ts",
-    "types",
-    "**/*.ts",
-    "**/*.tsx"
-  ]
+
 };
 
 export default nextConfig;
